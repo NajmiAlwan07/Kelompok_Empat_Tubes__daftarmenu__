951fde8ab46cb579efc1013d74d2ac4a86f72730
Najmi Alwan <najmialwan07@gmail.com>
2023-07-24 12:26:01 +0700

    commit daftar menu

---
+++ app/src/main/java/com/example/kelompok_empat_tubes/DaftarMenuMakanan.kt
@@ -0,0 +1,11 @@
+package com.example.kelompok_empat_tubes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DaftarMenuMakanan : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_daftar_menu_makanan)
+    }
+}
